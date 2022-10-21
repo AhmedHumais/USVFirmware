97ae668fc972a7085e95bea85e9521808e095a22
AhmedHumais <m.ahmedhumais@gmail.com>
2022-10-21 13:06:48 +0400

    added templated switch and inverted switches

--- src/usv_node.cpp
+++ src/usv_node.cpp
@@ -15,6 +15,7 @@ void UsvNode::run_sys(rclcpp::Node::SharedPtr node_p){
     auto usv_driver_blk = new UsvDriver(0); sys->addBlock(usv_driver_blk, "USV Driver");
     auto usv_rc_controller = new UsvRadioController(0); sys->addBlock(usv_rc_controller, "USV RC Controller");
     auto rc_override = new RCManualOverride(0);
+    auto manual_sw = new 
 
 
 }
